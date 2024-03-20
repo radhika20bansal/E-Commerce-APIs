b7f60859ccf5510ef4d43741f7ca9a0e5981301a
Radhika Bansal <radhikajanuary20@gmail.com>
2024-03-20 21:12:50 +0530

    database and middleware setup

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require("express");
+require('dotenv').config();
 const app = express();
+const connectDB = require('./db/connect');
+require('express-async-errors');
+const morgan = require('morgan');
 
-const port = process.env.PORT || 3000;
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+//middlewares
+app.use(morgan('tiny'));
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) => {
+    res.send('E-Commerce API');
+})
+
+//error middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`);
     });
