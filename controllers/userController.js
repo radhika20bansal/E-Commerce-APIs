08b61bb22434934a17f0b4c965ad8bfd479cbbb3
Radhika Bansal <radhikajanuary20@gmail.com>
2024-03-21 22:39:36 +0530

    update user password controller completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,24 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const customError = require('../errors');
+const customError = require("../errors");
 
 const getAllUsers = async (req, res) => {
-    console.log(req.user);
-  const users = await User.find({role: 'user'}).select('-password');
+  console.log(req.user);
+  const users = await User.find({ role: "user" }).select("-password");
   res.status(StatusCodes.OK).json({ users });
 };
 
 const getSingleUser = async (req, res) => {
-    const {id: userId} = req.params;
-    const user = await User.findOne({_id: userId}).select('-password');
-    if(!user){
-        throw new customError.NotFoundError(`User not found with id ${userId}`);
-    }
+  const { id: userId } = req.params;
+  const user = await User.findOne({ _id: userId }).select("-password");
+  if (!user) {
+    throw new customError.NotFoundError(`User not found with id ${userId}`);
+  }
   res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ user: "show current user" });
+  res.status(StatusCodes.CREATED).json({ user: req.user });
 };
 
 const updateUser = async (req, res) => {
@@ -26,7 +26,21 @@ const updateUser = async (req, res) => {
 };
 
 const updateUserPassword = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ user: "update user password" });
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new customError.BadRequestError(
+      "Please provide old and new passwords"
+    );
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+  const isCorrectPassword = user.comparePassword(oldPassword);
+  if (!isCorrectPassword) {
+    throw new customError.UnauthenticatedError("Invalid old password");
+  }
+  user.password = newPassword;
+  await user.save();
+  
+  res.status(StatusCodes.OK).json({msg: 'Success ! Password updated'});
 };
 
 module.exports = {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,9 +5,9 @@ const {authenticateUser, authorizePermissions} = require('../middleware/authenti
 const router = express.Router();
 
 router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllUsers);
-router.route('/showMe').get(showCurrentUser);
-router.route('/updateUser').patch(updateUser);
-router.route('/updateUserPassword').patch(updateUserPassword);
+router.route('/showMe').get(authenticateUser, showCurrentUser);
+router.route('/updateUser').patch(authenticateUser, updateUser);
+router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
 router.route('/:id').get(authenticateUser, getSingleUser);
 
 module.exports = router;
