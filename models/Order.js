544aae3b2edf866564a66093e4c9992540cfbd98
Radhika Bansal <radhikajanuary20@gmail.com>
2024-03-26 23:00:05 +0530

    Order Schema created

---
+++ models/Order.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+
+const CartItemsSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  product: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+});
+
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "failed", "paid", "delivered", "canceled"],
+      default: "pending",
+    },
+    orderItems: [CartItemsSchema],
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentIntentId: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
