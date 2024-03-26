1cfa1a1c68139bb54e73e33b23e581d1ed1e2b4e
Radhika Bansal <radhikajanuary20@gmail.com>
2024-03-26 14:44:24 +0530

    upload product image controller completed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const connectDB = require('./db/connect');
 require('express-async-errors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -16,6 +17,8 @@ const productRouter = require('./routes/productRoute');
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.static('./public'));
+app.use(fileUpload());
 
 //routes
 app.get('/', (req, res) => {
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const Product = require("../models/Product");
 const customError = require("../errors");
+const path = require("path");
 
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId;
@@ -16,7 +17,7 @@ const getAllProducts = async (req, res) => {
 const getSingleProduct = async (req, res) => {
   const { id: productId } = req.params;
   const product = await Product.findById({ _id: productId });
-  if(!product){
+  if (!product) {
     throw new customError.NotFoundError(`No product with id: ${productId}`);
   }
 
@@ -30,7 +31,7 @@ const updateProduct = async (req, res) => {
     req.body,
     { new: true, runValidators: true }
   );
-  if(!product){
+  if (!product) {
     throw new customError.NotFoundError(`No product with id: ${productId}`);
   }
 
@@ -40,16 +41,34 @@ const updateProduct = async (req, res) => {
 const deleteProduct = async (req, res) => {
   const { id: productId } = req.params;
   const product = await Product.findById({ _id: productId });
-  if(!product){
+  if (!product) {
     throw new customError.NotFoundError(`No product with id: ${productId}`);
   }
   await product.remove();
-  
-  res.status(StatusCodes.OK).json({ msg: 'Deleted Successfully!'});
+
+  res.status(StatusCodes.OK).json({ msg: "Deleted Successfully!" });
 };
 
-const uploadImage = (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "upload product image" });
+const uploadImage = async (req, res) => {
+  if (!req.files) {
+    throw new customError.BadRequestError("No file uploaded");
+  }
+  const productImage = req.files.image;
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new customError.BadRequestError("Please upload an image");
+  }
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new customError.BadRequestError(
+      "Please upload an image smaller than 1MB"
+    );
+  }
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+  res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
 };
 
 module.exports = {
